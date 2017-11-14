146f63a7e089881b5db924441fd2e0badce6ddd4
Earnest <earnestcpy@gmail.com>
2017-11-14 14:54:54 -0800

    Created Activities for Welcome screen, User Profile page and Exchange page, and updated User Profile layout and string xml

---
+++ app/src/main/java/com/example/jjone/icontec/ExchangeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jjone.icontec
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExchangeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange)
+        //this is a test
+    }
+}
---
+++ app/src/main/java/com/example/jjone/icontec/WelcomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jjone.icontec
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
