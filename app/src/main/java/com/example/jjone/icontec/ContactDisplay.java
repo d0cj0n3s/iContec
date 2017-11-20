8a112dd99d5a94f31ef7bfb1f167acde2e6dee12
Earnest <earnestcpy@gmail.com>
2017-11-19 17:39:42 -0800

    Merged Earnest's and Jason codes

--- app/src/main/java/com/example/jjone/icontec/ContactDisplay.java
+++ app/src/main/java/com/example/jjone/icontec/ContactDisplay.java
@@ -47,24 +47,24 @@ public class ContactDisplay extends AppCompatActivity
 
         while (cursor.moveToNext())
         {
-           id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-           name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-           Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                   ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
+            id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
             Log.i("My info",id + " = " + name);
             sb.append("Contact Id = " + id + "\n");
             sb.append("Contact Name = " + name + "\n");
 
             contacts.setText(sb.toString());
 
-           while(phoneCursor.moveToNext())
-           {
-               String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-               Log.i("My info", phoneNumber);
-           }
+            while(phoneCursor.moveToNext())
+            {
+                String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                Log.i("My info", phoneNumber);
+            }
 
-           Cursor emailCursor = resolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
-                   ContactsContract.CommonDataKinds.Email._ID + " = ?" ,new String[]{ id},null);
+            Cursor emailCursor = resolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Email._ID + " = ?" ,new String[]{ id},null);
 
             while(emailCursor.moveToNext())
             {
