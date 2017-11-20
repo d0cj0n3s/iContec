4d6bfb9de8fd7b083414907faeb2ce9ebe7bd33f
Jason Jones <jonesj115@mytru.ca>
2017-11-19 17:16:28 -0800

    Displaying the total contact user list

---
+++ app/src/main/java/com/example/jjone/icontec/ContactDisplay.java
@@ -0,0 +1,79 @@
+package com.example.jjone.icontec;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+
+public class ContactDisplay extends AppCompatActivity
+{
+
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    String id;
+    String name;
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_display);
+
+        StringBuilder sb = new StringBuilder("");
+
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+
+        TextView userInfo = findViewById(R.id.textView1);
+        TextView contacts = findViewById(R.id.textView2);
+
+        ContentResolver resolver = getContentResolver();
+
+        Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null,null,null, null);
+
+        while (cursor.moveToNext())
+        {
+           id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+           name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+           Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                   ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
+            Log.i("My info",id + " = " + name);
+            sb.append("Contact Id = " + id + "\n");
+            sb.append("Contact Name = " + name + "\n");
+
+            contacts.setText(sb.toString());
+
+           while(phoneCursor.moveToNext())
+           {
+               String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+               Log.i("My info", phoneNumber);
+           }
+
+           Cursor emailCursor = resolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+                   ContactsContract.CommonDataKinds.Email._ID + " = ?" ,new String[]{ id},null);
+
+            while(emailCursor.moveToNext())
+            {
+                String email = emailCursor.getString(emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                Log.i("My info", email);
+            }
+        }
+
+
+    }
+
+}
