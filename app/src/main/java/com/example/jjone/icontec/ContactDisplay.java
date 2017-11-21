9a7afe63d3d7b87511973fdbeaa1633e9b5ce972
Earnest <earnestcpy@gmail.com>
2017-11-20 20:03:38 -0800

    Created 2 items ListView on ContactDisplay Acrivity. Added OnitemClick function to each contact entry to display name and id.

--- app/src/main/java/com/example/jjone/icontec/ContactDisplay.java
+++ app/src/main/java/com/example/jjone/icontec/ContactDisplay.java
@@ -11,26 +11,39 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class ContactDisplay extends AppCompatActivity
 {
-
+    ListView contact_list;
     // Request code for READ_CONTACTS. It can be any number > 0.
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
     String id;
     String name;
 
-
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_display);
 
         StringBuilder sb = new StringBuilder("");
+
+        contact_list = (ListView) findViewById(R.id.list_contact);
+        final List<String[]> items_subitems = new LinkedList<String[]>();
 
         // Check the SDK version and whether the permission is already granted or not.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
@@ -38,8 +51,6 @@ public class ContactDisplay extends AppCompatActivity
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
         }
 
-        TextView userInfo = findViewById(R.id.textView1);
-        TextView contacts = findViewById(R.id.textView2);
 
         ContentResolver resolver = getContentResolver();
 
@@ -51,11 +62,12 @@ public class ContactDisplay extends AppCompatActivity
             name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
             Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                     ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{ id }, null);
-            Log.i("My info",id + " = " + name);
-            sb.append("Contact Id = " + id + "\n");
-            sb.append("Contact Name = " + name + "\n");
 
-            contacts.setText(sb.toString());
+            items_subitems.add(new String[]{ name, id} );
+            /*Log.i("My info",id + " = " + name);
+            sb.append("Contact Id = " + id + "\n");
+            sb.append("Contact Name = " + name + "\n");*/
+
 
             while(phoneCursor.moveToNext())
             {
@@ -72,6 +84,35 @@ public class ContactDisplay extends AppCompatActivity
                 Log.i("My info", email);
             }
         }
+        ArrayAdapter<String[]> items_subitem_Adapter = new ArrayAdapter<String[]>(this, android.R.layout.simple_list_item_2,android.R.id.text1, items_subitems)
+        {
+           @Override
+            public View getView(int position, View convertView, ViewGroup parent)
+           {
+               View view= super.getView(position, convertView, parent);
+
+               String[] entry = items_subitems.get(position);
+               TextView text1 = (TextView)view.findViewById(android.R.id.text1);
+               TextView text2 = (TextView)view.findViewById(android.R.id.text2);
+               text1.setText(entry[0]);
+               text1.setTextSize(20);
+               text2.setText(entry[1]);
+               text2.setVisibility(View.INVISIBLE);
+               return view;
+           }
+        };
+
+        contact_list.setAdapter(items_subitem_Adapter);
+        contact_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView contact_name = (TextView)view.findViewById(android.R.id.text1);
+                TextView contact_id = (TextView)view.findViewById(android.R.id.text2);
+                Toast.makeText(ContactDisplay.this, contact_name.getText().toString() + ": " + contact_id.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
 
     }
