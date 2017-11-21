5d93b0acccb061ea6afc092c0b184f4bcb18e429
Earnest <earnestcpy@gmail.com>
2017-11-20 22:23:35 -0800

    Nov 20th

--- app/src/main/java/com/example/jjone/icontec/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/jjone/icontec/WelcomeScreenActivity.java
@@ -1,9 +1,12 @@
 package com.example.jjone.icontec;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
@@ -11,6 +14,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     LinearLayout linear_layout_button;
     Animation uptodown;
     Animation downtoup;
+    Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,24 @@ public class WelcomeScreenActivity extends AppCompatActivity {
         linear_layout_icontect.setAnimation(uptodown);
         linear_layout_button.setAnimation(downtoup);
 
+        proceed = (Button) findViewById(R.id.btn_proceed);
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(WelcomeScreenActivity.this, ContactDisplay.class);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+    public void onBackPressed() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
 
     }
 }
